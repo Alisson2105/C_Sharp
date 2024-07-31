16c9dce857491abd9083b8dd3c21041275d8b4d2
Alisson_Souza <alisson.erga@outlook.com>
2024-07-30 22:26:43 -0300

    Primeiro Programa

---
+++ exercicios/ex000/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeiroExercicio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine(); // Adiciona esta linha para pausar a execução do console até que o usuário pressione Enter
+        }
+    }
+}
