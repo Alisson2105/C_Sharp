d580a20558c33860f580b9b784ceb4c1e82ddf3f
Alisson_Souza <alisson.erga@outlook.com>
2024-07-30 22:53:34 -0300

    Segundo Programa

---
+++ exercicios/ex001/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SegundoExercicio{
+    public class Program{
+        public static void Main(string[] args){
+            int num1, num2;
+
+            Console.Write(" - Digite um número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" - Digite outro número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($" - A soma Entre {num1} e {num2}, é: {num1 + num2}");
+
+            Console.WriteLine(" - Pressione enter para encerrar o programa.");
+            Console.ReadLine();
+        }
+    }
+}
