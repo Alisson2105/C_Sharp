68a8415ea2f729f553d1eaa165b6fd0ff8e08313
Alisson_Souza <alisson.erga@outlook.com>
2024-09-09 18:09:15 -0300

    Soma

---
+++ ex001/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculo{
+    public class Soma{
+        public static void Main(String[] args){
+            Double num1;
+            Double num2;
+
+            Console.Write("- Informe um número real (exemplo:10,5): ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("- Informe outro número real (exemplo:10,5): ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"A soma entre {num1} e {num2} é: {num1 + num2}");
+
+        }
+    }
+}
