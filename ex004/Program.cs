b03d075d42a9f8c72e13750e2210e13bf6c80af9
Alisson_Souza <alisson.erga@outlook.com>
2024-09-12 18:57:45 -0300

    Conversão de Temperatura

---
+++ ex004/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ex004{
+    public class Program{
+        public static void Main(string[] args){
+            double grau_celsius;
+
+            Console.Write(" - Informe a temperatura em graus Celsius (°C): ");
+            grau_celsius = Convert.ToDouble(Console.ReadLine());
+
+            double grau_fahrenheit = (9 * grau_celsius / 5) + 32;
+            Console.WriteLine($" - {grau_celsius}°C equivale a {grau_fahrenheit:f2}°F");
+        }
+    }
+}
