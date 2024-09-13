dc9dd73134da7a696164b858d587afc1f8f83d04
Alisson_Souza <alisson.erga@outlook.com>
2024-09-13 19:06:29 -0300

    Converte mi em km

---
+++ ex005/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ex005{
+    public class Program{
+        public static void Main(string[] args){
+            double milhas;
+
+            Console.Write(" - Informe o valor em milhas (mi): ");
+            milhas = Convert.ToDouble(Console.ReadLine());
+
+            double km = 1.60934 * milhas;
+            Console.WriteLine($" - {milhas} mi equivale a {km:F3} Km");
+        }
+    }
+}
