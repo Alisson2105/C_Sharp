28b9675da53a2be4573091f1fb5c156f00c2b77b
Alisson_Souza <alisson.erga@outlook.com>
2024-09-20 18:09:55 -0300

    Tabuada

--- ex007/Program.cs
+++ ex007/Program.cs
@@ -1,16 +1,23 @@
-﻿namespace ex007{
+﻿using System;
+
+namespace ex007{
     public class Program{
         public static void Main(string[] args){
             double km;
 
-            Console.Write(" - Informe um valor em km: ");
-            km = Convert.ToDouble(Console.ReadLine());
+            try{
+                Console.Write(" - Informe um valor em km: ");
+                km = Convert.ToDouble(Console.ReadLine());
 
-            double m = km * 1000;
-            double cm = km *100000;
+                double m = km * 1000;
+                double cm = km *100000;
 
-            Console.WriteLine($" - {km}km equivale a {m}m (metros).");
-            Console.WriteLine($" - {km}km equivale a {cm}cm (centímetros).");
+                Console.WriteLine($" - {km}km equivale a {m}m (metros).");
+                Console.WriteLine($" - {km}km equivale a {cm}cm (centímetros).");
+            }
+            catch{
+                Console.WriteLine(" - Por Favor, insira um número válido.");
+            }
         }
     }
 }
---
+++ ex008/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex009{
+    public class Program{
+        public static void Main(string[] args){
+            int num;
+
+           
+           try{
+                Console.Write(" - Informe um número para a tabuada: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                for (int controle = 0; controle <= 10; controle++){
+                    Console.WriteLine($" - {num} X {controle} = {controle * num}");
+                }
+            }
+            catch (FormatException){
+                Console.WriteLine(" - Por Favor, informe um número válido!");
+            }
+            catch (OverflowException){
+                Console.WriteLine(" - O número informado é muito grande!");
+            }
+            catch (Exception ex){
+                Console.WriteLine($" - Ocorreu um erro inesperado: {ex.Message}");
+            }
+        }
+    }
+}
