41c327d74df0cc4d3b852fad7317096feded8f5f
Alisson_Souza <alisson.erga@outlook.com>
2024-09-23 18:13:42 -0300

    Desafio da Idade

---
+++ ex009/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex009{
+    public class Program{
+        public static void Main(string[] args){
+            int idade;
+
+            Console.Write(" - Informe sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            int meses = idade*12;
+            int dias = idade*365;
+
+            Console.WriteLine($" - Sua idade em meses é: {meses}");
+            Console.WriteLine($" - Sua idade em dias é: {dias}");
+        }
+    }
+}
