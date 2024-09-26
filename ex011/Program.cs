085c295bb07ba8118737725554155fda3fdb96fb
Alisson_Souza <alisson.erga@outlook.com>
2024-09-26 17:45:00 -0300

    Maior número

---
+++ ex011/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ex011{
+    public class Program{
+        public static void Main(string[] args){
+            int num1;
+            int num2;
+
+            Console.Write(" - Informe o primeiro número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" - Informe o segundo número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2){
+                Console.WriteLine($" O número {num1} é o maior.");
+            }else if(num2 > num1){
+                Console.WriteLine($" - O número {num2} é o maior.");
+            }else{
+                Console.WriteLine($" - Os números {num1} e {num2} são iguais.");
+            }
+        }
+    }
+}
