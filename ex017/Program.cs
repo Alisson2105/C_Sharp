f465e7c6bfbe1a9d4d985970eb9c6f14d5cdf242
Alisson_Souza <alisson.erga@outlook.com>
2024-10-23 15:31:29 -0300

    Maior número

---
+++ ex017/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1, num2;
+
+Console.Write(" - Informe o primeiro número: ");
+while (!int.TryParse(Console.ReadLine(), out num1))
+{
+    Console.WriteLine(" - Entrada inválida. Por favor, insira um número inteiro.");
+}
+
+Console.Write(" - Informe o segundo número: ");
+while (!int.TryParse(Console.ReadLine(), out num2))
+{
+    Console.WriteLine(" - Entrada inválida. Por favor, insira um número inteiro.");
+}
+
+string resultado = num1 == num2 ? $" - Os números {num1} e {num2} são iguais." : $" - O maior número é {Math.Max(num1, num2)}.";
+
+Console.WriteLine(resultado);
